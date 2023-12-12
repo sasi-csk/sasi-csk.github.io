f6d3a5365cb93ff334651e88d35d0156ff3ff5b8
sasi-csk <152297857+sasi-csk@users.noreply.github.com>
2023-12-12 07:55:19 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,11 @@
+function getRecommendation() {
+  var selectedCrop = document.getElementById("cropForm").value;
+  
+  if (selectedCrop === "") {
+    alert("Invalid selection. Please choose a valid crop.");
+  } else {
+    // Proceed with the selected crop
+    alert(" Invalid selection. Please choose a valid crop " );
+    // Add any further actions or processing based on the selected crop
+  }
+}
